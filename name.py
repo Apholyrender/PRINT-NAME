79bc3da9e51e6f74dd406c218d364d10e06a7267
Afolabi Asogba <apholy2010@gmail.com>
2021-08-17 22:55:56 +0100

    Full name
    Full name updated

--- name.py
+++ name.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-name=input ("Asogba Afolabi M.")
+name=input ("Asogba Afolabi Mauwedo.")
 print (name)
-=======
-name=input ("Asogba Afolabi M.")
-print (name)
->>>>>>> f55e7be341ba3936bd8064f8f459e78e4ef77021
